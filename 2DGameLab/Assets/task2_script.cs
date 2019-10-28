985187f765afbbc604bd338442b860bd774ad99e
pawelrudnicki <kontakt.prudnicki@gmail.com>
2019-10-28 18:10:11 +0100

    zadanie 2

---
+++ 2DGameLab/Assets/task2_script.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class task2_script : MonoBehaviour {
+
+    public float maxSpeed = 4f;
+    public float maxRotation = 100f;
+    private Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 movingVector = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+
+        rb.AddForce(movingVector);
+
+        //rb.velocity = movingVector;
+
+        //rb.MovePosition(movingVector);
+
+    }
+}
