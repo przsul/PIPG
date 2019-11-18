c86120ab04dc8ceb7ffb10daf40ecc209bc1695d
pawelrudnicki <kontakt.prudnicki@gmail.com>
2019-11-18 17:59:33 +0100

    spawnowanie się przeciwników (oparte na logice fal)

--- 2DGameLab/Assets/EnemyAI.cs
+++ 2DGameLab/Assets/EnemyAI.cs
@@ -2,23 +2,17 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Enemy : MonoBehaviour {
-
+public class EnemyAI : MonoBehaviour
+{
     public float speed;
     public float stoppingDistance;
     public float retreatDistance;
 
-    public float startTimeBtwShots;
-    private float timeBtwShots;
-
-    public GameObject projectile;
     public Transform player;
 
     void Start()
     {
         player = GameObject.FindGameObjectWithTag("Player").transform;
-
-        timeBtwShots = startTimeBtwShots;
     }
 
     void Update()
@@ -31,20 +25,9 @@ public class Enemy : MonoBehaviour {
         {
             transform.position = this.transform.position;
         }
-        else if (Vector2.Distance(transform.position, player.position) < retreatDistance) 
+        else if (Vector2.Distance(transform.position, player.position) < retreatDistance)
         {
             transform.position = Vector2.MoveTowards(transform.position, player.position, -speed * Time.deltaTime);
-        }
-
-
-        if (timeBtwShots <= 0)
-        {
-            Instantiate(projectile, transform.position, Quaternion.identity);
-            timeBtwShots = startTimeBtwShots;
-        }
-        else
-        {
-            timeBtwShots -= Time.deltaTime;
         }
     }
 }
